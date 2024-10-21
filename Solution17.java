bf45f2e24e7e4490e9b474e816ed7faa25f13fa4
luyu <2233291301@qq.com>
2024-10-21 15:44:43 +0800

    first

--- Solution17.java
+++ Solution17.java
@@ -7,8 +7,8 @@ import java.util.*;
  * 例如，"ACGAATTCCG" 是一个 DNA 序列 。
  * 在研究 DNA 时，识别 DNA 中的重复序列非常有用。
  * 给定一个表示 DNA 序列 的字符串 s ，返回所有在DNA分子中出现不止一次的长度为10的序列(子字符串)。你可以按任意顺序返回答案。
- * 
- * 
+ *
+ *
  * 示例 1：
  * 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
  * 输出：["AAAAACCCCC","CCCCCAAAAA"]
@@ -16,33 +16,41 @@ import java.util.*;
  * 输入：s = "AAAAAAAAAAAAA"
  * 输出：["AAAAAAAAAA"]
  */
-class Solution {
-    static final int L = 10;
-    Map<Character, Integer> bin = new HashMap<Character, Integer>() {{
-        put('A', 0);
-        put('C', 1);
-        put('G', 2);
-        put('T', 3)
-    }};
 
+class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans[] = new ArrayList<String>();
-        int n = s.length;
-        if (n <= L) {
-            return ans;
+        List<String> result = new ArrayList<>();
+        if (s.length() < 10) {
+            return result; // 如果长度小于10，直接返回空列表
         }
-        int x = 0;
-        for (int i === 0; i < L - 1; ++i) {
-            x = (x << 2) | bin.get(s.charAt(i));
-        }
-        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
-        for (int i = 0; i <= n - L; ++i) {
-            x = ({x << 2} | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
-            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
-            if (cnt.get(x) == 2) {
-                ans.add(s.substring(i, i + L));
+
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String subStr = s.substring(i, i + 10);
+            if (seen.contains(subStr)) {
+                repeated.add(subStr); // 如果已经见过，加入重复集合
+            } else {
+                seen.add(subStr); // 否则加入见过集合
             }
         }
-        return ans;
+
+        result.addAll(repeated); // 将结果添加到列表中
+        return result;
+    }
+}
+
+class TestSolution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // 测试用例
+        System.out.println(solution.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")); // 输出: ["AAAAACCCCC", "CCCCCAAAAA"]
+        System.out.println(solution.findRepeatedDnaSequences("AAAAAAAAAAAAA")); // 输出: ["AAAAAAAAAA"]
+        System.out.println(solution.findRepeatedDnaSequences("ACGTACGTACGT")); // 输出: []
+        System.out.println(solution.findRepeatedDnaSequences("GAGAGAGAGAGAGAGA")); // 输出: ["GAGAGAGAGA"]
+        System.out.println(solution.findRepeatedDnaSequences("A")); // 输出: []
+        System.out.println(solution.findRepeatedDnaSequences("ACGTACGTACGTACGT")); // 输出: ["ACGTACGTAC"]
     }
 }
